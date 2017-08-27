b29b9061eeb476b142601695984a48ff2c689b4a
gcamara <guilhermeacamara@gmail.com>
2017-08-27 18:47:54 -0300

    highlighting icons, iterating over lists to create markers and places

--- js/app.js
+++ js/app.js
@@ -11,7 +11,9 @@ var data = {
     },
     locations : [
         {title: 'Plaza del Callao', position: {lat: 40.419856, lng: -3.705831}},
-        {title: 'Plaza de España', position: {lat: 40.423309, lng: -3.712211}}
+        {title: 'Plaza de España', position: {lat: 40.423309, lng: -3.712211}},
+        {title: 'Plaza de Colón', position: {lat: 40.424905, lng: -3.689134}},
+        {title: 'Parque de El Retiro', position: {lat: 40.415404, lng: -3.684504}}
     ]
 };
 
@@ -25,15 +27,6 @@ var Map = function() {
             // styles: styles
     });
 
-    data.locations.forEach(function (loc, idx) {
-        var marker = new google.maps.Marker({
-            position: loc.position,
-            title: loc.title,
-            animation: google.maps.Animation.DROP,
-            id: idx
-        });
-        marker.setMap(map);
-    })
 };
 
 /* === ViewModel ===*/
@@ -46,30 +39,61 @@ var ViewModel = function() {
     this.markers = ko.observableArray();
 
     data.locations.forEach(function (loc,idx) {
-        // var span = document.createElement('h6');
-        // span.innerHTML = loc.title;
-        // document.getElementById('display_locations').appendChild(span);
-
-        // var marker = new google.maps.Marker({
-        //     position: loc.position,
-        //     title: loc.title,
-        //     animation: google.maps.Animation.DROP,
-        //     id: idx
-        // });
-        //
-        // marker.setMap(map);
-        // self.markers.push(loc);
-        self.markers.push(new Marker(loc));
+        self.markers.push(new Marker(loc,idx));
     });
     // this.mapOptions = ko.observableArray();
+
+    this.filterOptions = function () {
+        alert('Filter this');
+    }
 
 };
 
 /* === Current Locations===*/
-var Marker = function (loc) {
+var Marker = function (loc,idx) {
+    var self = this;
     this.title = ko.observable(loc.title);
     this.position = ko.observable(loc.position);
     this.display=ko.observable(true);
+
+    function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+            '|40|_|%E2%80%A2',
+            new google.maps.Size(21, 34),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(10, 34),
+            new google.maps.Size(21,34));
+        return markerImage;
+    }
+
+    this.highlightedIcon = makeMarkerIcon('FFFF24');
+    this.defaultIcon = makeMarkerIcon('FF6146');
+
+    var marker = new google.maps.Marker({
+        position: loc.position,
+        title: loc.title,
+        animation: google.maps.Animation.DROP,
+        icon: self.defaultIcon,
+        id: idx
+    });
+    marker.setMap(map);
+
+    marker.addListener('mouseover', function() {
+        this.setIcon(self.highlightedIcon);
+    });
+    marker.addListener('mouseout', function() {
+        this.setIcon(self.defaultIcon);
+    });
+
+    this.highlightMarker = function () {
+        marker.setIcon(self.highlightedIcon);
+    };
+
+    this.defaultMarker = function () {
+        marker.setIcon(self.defaultIcon);
+    };
+
 };
 
 
